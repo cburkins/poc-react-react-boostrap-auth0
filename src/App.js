a6c33f26e14acb8ef6185ba02a40e6ed28d69aad
Chad Burkins <chad.burkins@gmail.com>
2019-12-31 16:16:40 -0500

    Added Auth0 to basic react-bootstrap Navbar menu.

--- src/App.js
+++ src/App.js
@@ -1,57 +1,65 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Custom Hook
+import { useAuth0 } from "./react-auth0-spa";
 
 export default function App() {
-  return (
-    <Router>
-      <div>
-        <Navbar bg="light">
-          {/* 1st Nav item, mr-auto pushes the 2nd Nav item to the right */}
-          <Nav className="mr-auto">
-            <Nav.Item>
-              <Nav.Link href="/">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="/about">About</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="/users">Users</Nav.Link>
-            </Nav.Item>
-          </Nav>
-          {/* 2nd Nav item, gets pushed to right because of mr-auto on first item */}
-          <Nav>Status: Connected</Nav>
-        </Navbar>
+    // useContext() to get access to global props/methods
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+    const isAuthenticatedStatus = isAuthenticated ? "Authenticated" : "Not Authenticated";
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+    // Use custom hook, essentially grabs the "loading" via useContext()
+    // loading=true means that we are still waiting to get our authentication state from Auth0
+    const { loading } = useAuth0();
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className="App">
+            <HashRouter>
+                <div>
+                    <Navbar bg="light">
+                        {/* 1st Nav item, mr-auto pushes the 2nd Nav item to the right */}
+                        {/* prettier-ignore */}
+                        <Nav className="mr-auto">
+                            <Nav.Item><Nav.Link as={Link} to="/">Home</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link as={Link} to="/about">About</Nav.Link></Nav.Item>
+                            <Nav.Item><Nav.Link as={Link} to="/users">Users</Nav.Link></Nav.Item>
+                        </Nav>
+                        {/* 2nd Nav item, gets pushed to right because of mr-auto on first item */}
+                        <Nav>Status: {isAuthenticatedStatus}</Nav>
+                        <Nav className="ml-3">
+                            {!isAuthenticated && <button onClick={() => loginWithRedirect({})}>Log in</button>}
+                            {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+                        </Nav>
+                    </Navbar>
+                    {/* A <Switch> looks through its children <Route>s and renders first that matches current URL. */}
+                    <Switch>
+                        <Route path="/about" component={About} />
+                        <Route path="/users" component={Users} />
+                        <Route path="/" component={Home} />
+                    </Switch>
+                </div>
+            </HashRouter>
+        </div>
+    );
 }
 
 function Home() {
-  return <h2>Home</h2>;
+    return <h2>Home</h2>;
 }
 
 function About() {
-  return <h2>About</h2>;
+    return <h2>About</h2>;
 }
 
 function Users() {
-  return <h2>Users</h2>;
+    return <h2>Users</h2>;
 }
